118191676e239e78009b7e1cc599d40946029801
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2022-12-16 19:18:01 -0300

    header completo

---
+++ script.js
@@ -0,0 +1,15 @@
+function mudoutamanho(){
+    if(innerWidth >= 768){
+        itens.style.display = 'block'
+    }else{
+        itens.style.display = 'none'
+    }
+}
+
+function clickmenu(){
+    if (itens.style.display == 'block'){
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'block'
+    }
+}
