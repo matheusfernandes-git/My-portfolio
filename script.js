8797f6c1876b0392f29669ebce5de666f9f376ae
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-08-28 21:10:01 -0300

    organizing the images and adding new angular project

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-const menu_items = document.querySelectorAll('nav .mainMenu li a');
+const mainMenu = document.querySelector(".mainMenu");
+const closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".openMenu");
+const menu_items = document.querySelectorAll("nav .mainMenu li a");
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
 
-// close menu when you click on a menu item 
-menu_items.forEach(item => {
-    item.addEventListener('click',function(){
-        close();
-    })
-})
+// close menu when you click on a menu item
+menu_items.forEach((item) => {
+  item.addEventListener("click", function () {
+    close();
+  });
+});
 
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
 }
 
-function close(){
-    mainMenu.style.top = '-150%';
+function close() {
+  mainMenu.style.top = "-150%";
 }
 
 // Translate button
@@ -28,12 +28,11 @@ function close(){
 
 //Top button
 
-const btnTop = document.getElementById('btn-top');
-window.addEventListener('scroll', () => {
-    if(window.scrollY == 0){
-        btnTop.classList.remove('visible');
-    }else{
-        btnTop.classList.add('visible');
-    }
-})
-
+const btnTop = document.getElementById("btn-top");
+window.addEventListener("scroll", () => {
+  if (window.scrollY == 0) {
+    btnTop.classList.remove("visible");
+  } else {
+    btnTop.classList.add("visible");
+  }
+});
